c782693f3c90846466bd535cfe56044646dc83d6
h4rm41n <mail.ahmad.harmain@gmail.com>
2018-10-08 13:39:11 +0700

    perubahan di file latihan 1

---
+++ latihan_1.py
@@ -0,0 +1,59 @@
+a = 4
+A = 5
+
+kata = "python"
+kalimat = 'ini adalah string'
+paragraf = """ini adalah string
+         ini adalah string
+         ini adalah string
+         ini adalah string"""
+
+var_1 = True
+var_2 = "7"
+var_2_angka = int(var_2)
+# print(type(var_2_angka))
+
+var_tuple = ("Laki","Perempuan")
+var_list = [
+   2,
+   {
+      'nama'   :'Ahmad',
+      'alamat' :'Selong',
+      'kelamin':'Laki'
+   },
+]
+
+var_dict = {
+   'nama':'Ahmad',
+   'alamat':'Selong',
+   'kelamin':'Laki'
+}
+
+var_dic_list = [
+   {
+      'nama':'Ahmad',
+      'alamat':'Selong',
+      'kelamin':'Laki'
+   },
+   {
+      'nama':'Ahmad1',
+      'alamat':'Selong1',
+      'kelamin':'Laki1'
+   },
+   {
+      'nama':'Ahmad2',
+      'alamat':'Selong2',
+      'kelamin':'Laki2'
+   }  
+]
+
+dict_contoh = {
+   'nama':'Ahmad2',
+   'alamat':'Selong2',
+   'kelamin':'Laki2'
+}
+list_ = [1,4,8]
+list_.append(dict_contoh)
+for i in range(0,len(list_)):print list_[i]
+   
+      
